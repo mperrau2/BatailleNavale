2ceeefcadc96ef7d22d3da875dc20b9cfe63cfdb
mperrau2 <perraud.maxime@laposte.net>
2014-11-27 08:13:48 +0100

    Create Appli.java

---
+++ Appli.java
@@ -0,0 +1,14 @@
+class Appli {
+ 
+  public static void main(String[] args) {
+ 
+    javax.swing.SwingUtilities.invokeLater( new Runnable() {
+ 
+
+	public void run() {
+    	  Fenetre f = new Fenetre();
+      }
+ 
+    });
+  }
+}
